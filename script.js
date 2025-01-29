be03d6812e8c512865c53f6f3d51ef04ae41ad2e
Dawiz0p <shakespearama@protonmail.com>
2025-01-28 23:43:01 -0500

    next is making calculator interactive for user.

---
+++ script.js
@@ -0,0 +1,24 @@
+const container = document.querySelector('#container');
+const button = document.querySelector('button');
+
+
+
+
+function add(num1, num2){
+    console.log(num1 + num2);
+}
+
+function subtract(num1, num2){
+    console.log(num1 - num2);
+}
+
+function multiply(num1, num2){
+    console.log(num1 * num2);
+}
+
+function divide(num1, num2){
+    console.log(num1 / num2);
+}
+
+
+
