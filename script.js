003826ce8c901a62552616f4ab77202788698009
Dawiz0p <shakespearama@protonmail.com>
2025-02-01 00:23:37 -0500

    addEventListener to button click logs the button to console.

--- script.js
+++ script.js
@@ -1,18 +1,24 @@
-const buttons = document.querySelectorAll('.item');
+const numbers = document.querySelectorAll('.op');
+const operators = document.querySelectorAll('.opr')
+const buttons = document.querySelectorAll('button');
 const display = document.querySelector('.item0');
 
-console.log(buttons[3]);
 
 
 
-// function when button is clicked to display num
-let number = 0;
-buttons.forEach(bt => {
-    bt.addEventListener('click', (e) => {
-        number = e.target.innerHTML;
-        display.innerHTML = number;
-    });
-});
+
+
+// function when buttons are clicked - logs button to console
+function clickButton(){
+    buttons.forEach(bt => {
+        bt.addEventListener('click', () => {
+           console.log(bt)
+           
+           
+        })
+    })
+}
+clickButton();
 
 
 
