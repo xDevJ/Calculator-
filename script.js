8a0cc354f09265fe46ca7518e485a115404fc6d3
Dawiz0p <shakespearama@protonmail.com>
2025-02-02 12:24:20 -0500

    started with writing function for all buttons clicked. tried to figure out the algorithm of a calculator was a bit tough. written all function for each value of button being clicked.

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 const buttons = document.querySelectorAll('button');
 const display = document.querySelector('.item0');
-let calcValue = 0;
+let calcValue = '0';
 let firstNumber = null;
 let secondNumber = null;
 let firstOperator = null;
@@ -11,7 +11,9 @@ let result = null;
 function updateDisplay(){
     const display = document.getElementById('display');
     display.innerHTML = calcValue;
-    
+    if(calcValue.length > 9){
+        display.innerHTML = calcValue.substring(0, 9);
+    }
 }
 updateDisplay();
 
@@ -43,21 +45,79 @@ clickButton();
 
 // function when numbers are clicked
 function clickNumber(number){
-  
+  if(firstOperator === null){
+    if(calcValue === '0' || calcValue === 0){
+        calcValue = number;
+    } else if(calcValue === firstNumber){
+        calcValue = number
+    } else {
+        calcValue += number;
+    }
+  } else {
+    if(calcValue === firstNumber){
+        calcValue = number;
+    } else {
+        calcValue += number;
+    }
+  }
 }
 
 
 // function when operators are clicked
 function clickOperator(operator){
-   
+   if(firstOperator != null && secondOperator === null){
+    secondOperator = operator;
+    secondNumber = calcValue;
+    result = operate(Number(firstNumber), Number(secondNumber), firstOperator);
+    calcValue = roundAccurately(result, 15).toString();
+    firstNumber = calcValue;
+    result = null;
+   } else if(firstOperator != null && secondOperator != null){
+    secondNumber = calcValue;
+    result = operate(Number(firstNumber), Number(secondNumber), secondOperator);
+    calcValue = roundAccurately(result, 15).toString();
+    firstNumber = calcValue;
+    result = null;
+   } else {
+    firstOperator = operator;
+    firstNumber = calcValue;
+   }
     
 }
 
 // function when equal sign is clicked
 
 function inputEquals(){
-    
- }
+    if(firstOperator === null){
+        calcValue = calcValue;
+    } else if(secondOperator != null){
+        secondNumber = calcValue;
+        result = operate(Number(firstNumber), Number(secondNumber), secondOperator);
+        if(result = 'err'){
+            displayValue = 'err'
+        } else {
+            calcValue = roundAccurately(result, 15).toString();
+            firstNumber = calcValue;
+            secondNumber = null;
+            firstOperator = null;
+            secondOperator = null;
+            result = null;
+        }
+    } else {
+        secondNumber = calcValue;
+        result = operate(Number(firstNumber), Number(secondNumber), firstOperator);
+        if(result === 'err'){
+            calcValue = 'err';
+        } else {
+            calcValue = roundAccurately(result, 15).toString();
+            firstNumber = calcValue;
+            secondNumber = null;
+            firstOperator = null;
+            secondOperator = null;
+            result = null;
+        }
+    }
+}
     
 
 
@@ -87,3 +147,8 @@ function operate(x, y, op){
     }
  
 } 
+
+
+function roundAccurately(num, places){
+    return parseFloat(Math.round(num + 'e' + places) + 'e-' + places);
+}
